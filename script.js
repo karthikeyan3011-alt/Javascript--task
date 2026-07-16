e440003a19b01fbcb19b0a93a773094cc708a04e
karthikeyan3011-alt <karthi3001keyan@gmail.com>
2026-07-17 00:47:34 +0530

    Add files via upload
    javascript examples task

---
+++ script.js
@@ -0,0 +1,199 @@
+// Task 1
+
+const companyName = "Stackly";
+
+let employeeName = prompt("Enter Employee Name:");
+
+let employeeID = "STK-26-3928";
+
+console.log("Company : " + companyName); // Stackly
+console.log("Employee Name : " + employeeName); // Karthick
+console.log("Employee ID : " + employeeID); // STK-26-3928
+
+
+// Task 2
+
+const collegeName = "Hindustan College of Arts and science";
+
+let studentName = prompt("Enter Student Name:");
+
+let course = prompt("Enter Course:");
+
+console.log("College : " + collegeName); // Hindustan College of Arts and science
+console.log("Student : " + studentName); // Karthick
+console.log("Course : " + course); // BCA
+
+
+// Task 3
+
+const restaurantName = "Radisson blu ";
+
+let customerName = prompt("Enter Customer Name:");
+
+let foodName = prompt("Enter Food Name:");
+
+let quantity = prompt("Enter Quantity:");
+
+console.log("Restaurant : " + restaurantName); // Radisson blu 
+console.log("Customer : " + customerName); // Karthick 
+console.log("Food : " + foodName); // Grilled Fish
+console.log("Quantity : " + quantity); // 1
+
+
+// Task 4
+
+const hospitalName = "City Hospital";
+
+let patientName = prompt("Enter Patient Name:");
+
+let tokenNumber = "10";
+
+console.log("Hospital : " + hospitalName); // City hospital
+console.log("Patient : " + patientName); // Karthick
+console.log("Token Number : " + tokenNumber); // 10
+
+
+// Task 5
+
+const theatreName = "INOX";
+
+let movieName = prompt("Enter Movie Name:");
+
+let customerName1 = prompt("Enter Customer Name:");
+
+let seatNumber1 = "C15";
+
+console.log("Theatre : " + theatreName); // INOX
+console.log("Movie : " + movieName); // Master
+console.log("Customer : " + customerName1); // Karthick
+console.log("Seat Number1 : " + seatNumber); // B16
+
+
+// Task 6
+
+const shopName = "Golden Mobiles";
+const customerName2 = prompt("Enter your name:");
+const mobileModel = prompt("Enter the mobile model:");
+
+let price1 = prompt("Enter the mobile price:");
+
+console.log("Shop Name: " + shopName);      // Golden Mobiles
+console.log("Customer Name2: " + customerName); // Karthick
+console.log("Mobile Model: " + mobileModel);   // OPPO F31
+console.log("Price1: ₹" + price);               // ₹45000
+
+
+// Task 7
+
+const gymName = "Gravity fitness Studio";
+const memberName = prompt("Please enter your name:");
+
+let membershipFee = prompt("Please enter your membership fee:");
+
+console.log("Gym Name: " + gymName); // Gravity fitness Studio
+console.log("Member Name: " + memberName); // Karthick
+console.log("Membership Fee: ₹" + membershipFee); // ₹3000
+
+
+// Task 8
+
+const bankName = "State Bank of India";
+const userName = prompt("Please enter your name:");
+
+console.log("Bank Name: " + bankName); // State Bank of India
+console.log("Welcome " + userName); // Karthick
+
+
+// Task 9
+
+const travelsName = "KPN Travels";
+const passengerName = prompt("Please enter your name:");
+
+let seatNumber = prompt("Please enter your seat number:");
+
+console.log("Travels Name: " + travelsName); // KPN Travels
+console.log("Passenger Name: " + passengerName); // Karthick
+console.log("Seat Number: " + seatNumber); // 24
+
+
+// Task 10
+
+const websiteName = "Amazon";
+const productName = prompt("Please enter the product name:");
+const customerName3 = prompt("Please enter your name:");
+
+let price = prompt("Please enter the product price:");
+
+console.log("Website Name: " + websiteName); // Amazon
+console.log("Product Name: " + productName); // LED TV
+console.log("Customer Name3: " + customerName); // Karthick
+console.log("Price2: ₹" + price); // ₹50000
+
+
+// Task 11
+
+console.log("Registration Successful"); // Registration Successful
+
+
+// Task 12
+
+console.error("Payment Failed"); // Payment Failed
+
+
+// Task 13
+
+console.warn("Battery Low"); // Battery Low
+
+
+// Task 14
+
+console.log("Loading..."); // Loading...
+console.log("Welcome"); // Welcome
+
+console.clear(); //
+
+
+// Task 15
+
+alert("Welcome to Amazon"); // Welcome to Amazon
+
+
+// Task 16
+
+let logout = confirm("Do you want to logout?");
+
+console.log(logout); // true - if user clicks ok // false - if user clicks cancel
+
+
+// Task 17 
+
+let city = prompt("Please enter your city:");
+
+console.log("Your City : " + city); // Coimbatore
+
+
+// Task 18
+
+var product = "Laptop";
+
+product = "Mobile";
+
+var product = "Tablet";
+
+console.log(product); // Tablet
+
+
+// Task 19
+
+let salary = 25000;
+
+salary = 30000;
+
+console.log(salary); // 30000
+
+
+// Task 20
+
+const company = "Microsoft";
+
+console.log(company); // Microsoft
